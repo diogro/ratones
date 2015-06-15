cac2a216a07fe8f8db21cd78a7eddc6ff75d07dd
diogro <diogro@gmail.com>
2015-06-15 17:18:26 -0300

    mock report

--- R/bayseanStats.R
+++ R/bayseanStats.R
@@ -1,4 +1,7 @@
 source("R/run_ratones_MCMCglmm_P.R")
+
+vectorCor <- function(x, y) t(Normalize(x)) %*% Normalize(y)
+
 ML_R2 = ldply(main.data, function(x) CalcR2(x$cov.matrix))
 
 r_models %>% llply(function(x) x$Ps) %>% ldply(function(x) adply(x, 1, CalcR2)) %>%
@@ -8,11 +11,13 @@ r_models %>% llply(function(x) x$Ps) %>% ldply(function(x) adply(x, 1, CalcR2)) 
   geom_point(data = ML_R2, color = 'red', aes(.id, V1)) + 
   theme_bw() + labs(y = expression(R^2))
 
-mcmc_stats = tbl_df(ldply(r_models, function(x) adply(x$Ps, 1, MeanMatrixStatistics), .parallel = TRUE))
+# mcmc_stats = tbl_df(ldply(r_models, function(x) adply(x$Ps, 1, MeanMatrixStatistics), .parallel = TRUE))
+# save(mcmc_stats, file = "./Rdatas/mcmc_stats")
+load("./Rdatas/mcmc_stats")
 mcmc_stats %>% select(.id, MeanSquaredCorrelation, flexibility, evolvability) %>% melt %>%
   separate(.id, c( 'treatment', 'strain')) %>% 
-  ggplot(aes(treatment, value, group = interaction(treatment, strain, variable), color = strain)) + geom_boxplot() +
-  facet_wrap(~variable, scale = 'free') + theme_bw()
+  ggplot(aes(treatment, value, group = interaction(treatment, strain, variable), fill = strain)) + geom_boxplot() +
+  facet_wrap(~variable, scale = 'free') + theme_bw() -> global_stats_plot
 
 x = main.data[[2]]
 delta_Zs <- llply(main.data, function(x) x$ed.means - main.data$control.control$ed.means)
@@ -27,24 +32,27 @@ directionalVariation <- function(cov.matrix, strain){
   beta = c(1, rep(0, 35))
   #eval1 <- eigen(cov.matrix)$value[1]
   #eval1_ <- (eigen(cov.matrix[-1, -1])$value)
-  #mat.stat <- MeanMatrixStatistics(cov.matrix, parallel = TRUE)
+  flexibility <- Flexibility(cov.matrix)
+  evolvability <- Evolvability(cov.matrix)
   data.frame(corDZDZ = vectorCor(cov.matrix %*% beta, delta_Z),
-             flex_DZ = Flexibility(cov.matrix, Normalize(delta_Z)),#/mat.stat['flexibility'],
-             flex_beta = Flexibility(cov.matrix, Normalize(beta)),#/mat.stat['flexibility'], 
-             flex_plsr_strain = Flexibility(cov.matrix[-1,-1], Normalize(plsr_strain)),#/mat.stat['flexibility'], 
-             flex_plsr_control = Flexibility(cov.matrix[-1,-1], Normalize(plsr_control)),#/mat.stat['flexibility'], 
-             Evol_DZ = Evolvability(cov.matrix, Normalize(delta_Z)),#/eval1,
-             #DZpc1 = abs(vectorCor(delta_Z,
-             #                       eigen(cov.matrix)$vector[,1])),
+             corDZplsrDZ = vectorCor(cov.matrix[-1,-1] %*% plsr_strain, delta_Z[-1]),
+             flex_DZ = Flexibility(cov.matrix, Normalize(delta_Z))/flexibility,
+             flex_beta = Flexibility(cov.matrix, Normalize(beta))/flexibility, 
+             #flex_plsr_strain = Flexibility(cov.matrix[-1,-1], Normalize(plsr_strain)),#/mat.stat['flexibility'], 
+             #flex_plsr_control = Flexibility(cov.matrix[-1,-1], Normalize(plsr_control)),#/mat.stat['flexibility'], 
+             Evol_DZ = Evolvability(cov.matrix, Normalize(delta_Z))/evolvability,
+             DZpc1 = abs(vectorCor(delta_Z,
+                                    eigen(cov.matrix)$vector[,1])),
              #corBetaBetaS = vectorCor(beta, beta_s),
              #corBetaBetaNN = vectorCor(beta, beta_NN),
              #corDZbetaS = vectorCor(delta_Z, beta_s),            
-             Evol_beta = Evolvability(cov.matrix, Normalize(beta)),#/eval1, 
+             Evol_beta = Evolvability(cov.matrix, Normalize(beta))/evolvability
              
-             Evol_plsr_strain = Evolvability(cov.matrix[-1, -1], Normalize(plsr_strain)),#/eval1_, 
+             #Evol_plsr_strain = Evolvability(cov.matrix[-1, -1], Normalize(plsr_strain)),#/eval1_, 
              
-             Evol_plsr_control = Evolvability(cov.matrix[-1, -1], Normalize(plsr_control)),#/eval1_, 
-             normDZ = Norm(delta_Z))
+             #Evol_plsr_control = Evolvability(cov.matrix[-1, -1], Normalize(plsr_control)),#/eval1_, 
+             #normDZ = Norm(delta_Z))
+  )
 }
 
 treatment <- ldply(r_models[-1], function(model) adply(model$Ps, 1, 
@@ -60,8 +68,12 @@ stats$fe = '0'
 stats$fe[grep('flex', as.character(stats$variable))] <- 'flex'
 stats$fe[grep('Evol', as.character(stats$variable))] <- 'Evol'
 
-stats %>% filter(variable != 'normDZ') %>% 
-  ggplot(., aes(variable, value, group = interaction(type, variable, .id), color = type)) + geom_boxplot() + facet_grid(.id~fe, scales = "free")
+stats %>% separate(.id, c( 'treatment', 'strain')) %>% filter(variable == 'DZpc1') %>% filter(type == "treatment") %>% head
+  ggplot(aes(treatment, value, group = interaction(treatment, strain, type), fill = strain)) + geom_boxplot() + theme_bw()
 
-stats %>% filter(variable != 'normDZ') %>% 
-  ggplot(., aes(.id, value, group = interaction(type, variable, .id), color = type)) + geom_boxplot() + facet_wrap(~variable, scales = "free")
+testplsrHip <- function (x) {
+  hips = matrix(c(as.numeric(x$ed.means - main.data[[1]]$ed.means > 0), 
+                  as.numeric(x$ed.means - main.data[[1]]$ed.means < 0)), ncol = 2)
+  TestModularity(cov2cor(x$cov.matrix), hips)
+}
+ldply(main.data, testplsrHip)
