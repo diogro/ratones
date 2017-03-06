3d512a92692bba6dee59df38d4b70b9ca2cf3894
Anna Penna <anna.penna@usp.com>
2017-03-05 23:15:24 -0300

    colored and ordered plots rs projection

--- R/KrzSubspace.R
+++ R/KrzSubspace.R
@@ -57,8 +57,9 @@ krz_subspace_plot = rbind(cbind(rank = 1:35, as.data.frame(observed), type = "Ob
   ggplot(aes(x = rank, y = mean, linetype = type, color = type)) + 
   geom_point(position = position_dodge(width = 0.5)) + 
   geom_linerange(aes(ymin = lower, ymax = upper), position = position_dodge(width = 0.5)) +
-  labs(y = "Eigenvalues of H", x = "Eigenvectors of H") +
-  theme(legend.position = c(0.75, 0.9), text = element_text(size = 20)) + 
+  labs(y = "Eigenvalues of H", x = "Eigenvectors of H") + background_grid(major = 'x', minor = "none") + 
+  panel_border() + theme(panel.grid.minor = element_line(colour = "grey", linetype = "dotted", size =0.2))+ 
+  theme(legend.position = c(0.75, 0.9), text = element_text(size = 18)) + 
   scale_colour_grey("", start = 0, end = 0.6) + scale_linetype(guide = "none")
   krz_subspace_plot
 save_plot("~/Dropbox/labbio/Shared Lab/Ratones_shared/krz_subspace_(figure2?).png", krz_subspace_plot, base_aspect_ratio = 1.3, base_height = 4.8)
@@ -66,42 +67,54 @@ save_plot("~/Dropbox/labbio/Shared Lab/Ratones_shared/krz_subspace_(figure2?).pn
 dimnames(p_matrices)[[3]] = Gnames
 
 rs_projection = RSProjection(p_matrices)
-rs_projection_plot_full = PlotRSprojection(rs_proj = rs_projection, p_matrices, p = 0.95, num_pc = 35, ncols = 5)
+rs_projection_plot_full = PlotRSprojection(rs_proj = rs_projection, cov.matrix.array = p_matrices, num_pc = 35, p = 0.95, ncols = 5)
+rs_projection_plot_full
 save_plot("~/Dropbox/labbio/Shared Lab/Ratones_shared/rs_projection_(full_SI).png", rs_projection_plot_full, base_aspect_ratio = 1.3, base_height = 4.8, ncol = 5, nrow = 7)
-rs_projection_plot = PlotRSprojection(rs_proj = rs_projection, p_matrices, p = 0.95, num_pc = 9, ncols = 3)
+
+PlotRSprojection_rata <- function( rs_proj = rs_projection, cov.matrix.array = p_matrices, num_pc = 8, p = 0.95, ncols = 5)
+{
+n <- dim(cov.matrix.array)[[1]]
+evecs = t(rs_proj$eig.R$vectors)
+R_eigen_projection <- apply(cov.matrix.array, 3:4, function(mat) diag(evecs %*% 
+                                                                        mat %*% t(evecs)))
+R_eigen_projection <- aperm(R_eigen_projection, c(3, 2, 1))
+colnames(R_eigen_projection) <- dimnames(cov.matrix.array)[[3]]
+HPD.int <- aaply(R_eigen_projection, 3, function(proj) HPDinterval(as.mcmc(proj), 
+                                                                   prob = p))
+HPD.int <- aperm(HPD.int, c(2, 3, 1))
+dimnames(HPD.int) = list(dimnames(cov.matrix.array)[[3]], 
+                         NULL, NULL)
+dat = adply(HPD.int, 1:3)
+names(dat) = c("Population", "interval", "trait", "value")
+dat$trait <- as.numeric(dat$trait)
+if(!is.null(num_pc))  dat %<>% filter(trait <=num_pc)
+dat$trait <- as.factor(dat$trait)
+dat = dcast(dat, as.formula("Population+trait~interval"))
+names(dat) = c("Population", "trait", "lower", "upper")
+dat$trait = paste0("E", dat$trait)
+order.list = paste0("E", 1:num_pc)
+dat$trait = factor(dat$trait, levels = order.list)
+dat$Population <- as.factor(dat$Population)
+dat$Population = factor(dat$Population, levels = c("control.t", "upwards.h'", "upwards.s'","downwards.h", "downwards.s"))
+dat$mean = rowMeans(cbind(dat$upper, dat$lower))
+myPalette <- viridis(5)
+
+plot = ggplot(dat, aes_string( colour= "Population", x = "Population", y = "mean")) + geom_point() + 
+  geom_errorbar(aes_string(ymin = "lower", ymax = "upper"), size = 1 ) + 
+  scale_color_manual(values = myPalette, name = "Line", labels = c("Control t", "Upwards h'", "Upwards s'", "Downwards h", "Downwards s")) +
+  theme(axis.text.x = element_text(hjust = 1)) + background_grid() + 
+  theme(legend.position = c(0.85, 0.15), text = element_text(size = 18)) +
+  scale_x_discrete("Lines", labels = c("control.t" = "t","upwards.h'" = "h'", "upwards.s'" = "s'","downwards.h" = "h","downwards.s" = "s")) +
+  facet_wrap(~trait, ncol = 3, scales = "free_y") + panel_border() + ylab("Phenotypic Variance") 
+
+return(plot)
+}
+
+rs_projection_plot = PlotRSprojection_rata(rs_proj = rs_projection, p_matrices, p = 0.95, num_pc = 8, ncols = 3)
 save_plot("~/Dropbox/labbio/Shared Lab/Ratones_shared/rs_projection_(figure2?).png", rs_projection_plot, base_aspect_ratio = 1.3, base_height = 4.8)
 
-figure_2_review = plot_grid(krz_subspace_plot, rs_projection_plot, labels = c("A", "B"))
+figure_2_review = plot_grid(krz_subspace_plot, rs_projection_plot, labels = c("A", "B"), ncol = 2, rel_heights = c(1,1.3))
 save_plot("~/Dropbox/labbio/Shared Lab/Ratones_shared/figure2_review.png", figure_2_review, base_aspect_ratio = 1.3, base_height = 4.8, ncol = 2)
 
-PlotRSprojection = function (rs_proj, cov.matrix.array, p = 0.95, num_pc = 5, ncols = 5) {
-  n <- dim(cov.matrix.array)[[1]]
-  evecs = t(rs_proj$eig.R$vectors)
-  R_eigen_projection <- apply(cov.matrix.array, 3:4, function(mat) diag(evecs %*% 
-                                                                          mat %*% t(evecs)))
-  R_eigen_projection <- aperm(R_eigen_projection, c(3, 2, 1))
-  colnames(R_eigen_projection) <- dimnames(cov.matrix.array)[[3]]
-  HPD.int <- aaply(R_eigen_projection, 3, function(proj) HPDinterval(as.mcmc(proj), 
-                                                                     prob = p))
-  HPD.int <- aperm(HPD.int, c(2, 3, 1))
-  dimnames(HPD.int) = list(dimnames(cov.matrix.array)[[3]], 
-                           NULL, NULL)
-  dat = adply(HPD.int, 1:3)
-  names(dat) = c("Line", "interval", "trait", "value")
-  dat = dcast(dat, as.formula("Line+trait~interval"))
-  names(dat) = c("Line", "trait", "lower", "upper")
-  dat$trait = paste0("PC", dat$trait)
-  order.list = paste0("PC", 1:n)
-  dat$trait = factor(dat$trait, levels = order.list)
-  dat$mean = rowMeans(cbind(dat$upper, dat$lower))
-  plot = ggplot(filter(dat, trait %in% paste0("PC", 1:num_pc)), aes_string("Line", "mean")) + geom_point() + 
-    geom_errorbar(aes_string(ymin = "lower", ymax = "upper")) + 
-    theme_bw() + theme(axis.text.x = element_text(angle = 45, 
-                                                  hjust = 1)) + 
-    ylab("Phenotypic Variance") + facet_wrap(~trait, ncol = ncols, scales = "free_y") +
-    scale_x_discrete(labels  = c("Control t", "Upwards h'", "Upwards s'", "Downwards h", "Downwards s")) +
-                       theme(text = element_text(size = 15))
-                     
 
-  return(plot)
-}
+rs_projection_plot <- plot
