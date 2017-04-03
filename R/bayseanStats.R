846093870279cabf3b631afe4eb4556a5870c9ca
diogro <diogro@gmail.com>
2017-04-03 18:54:32 -0300

    full beta delta Z response

--- R/bayseanStats.R
+++ R/bayseanStats.R
@@ -47,8 +47,8 @@ evolDZ <- stats %>% dplyr::select(-X1) %>% ggplot(aes(evolDZ, group = .id, fill 
 condevolDZ <- stats %>% dplyr::select(-X1) %>% ggplot(aes(condevolDZ, group = .id, fill = .id)) + geom_density(alpha = 0.5) +  labs(x = "Scaled directional\n conditional evolvability", y = "Density") + scale_fill_manual(values = viridis(5), name = "Line", labels = c("Control t", "Upwards h'", "Upwards s'", "Downwards h", "Downwards s")) + background_grid(major = 'x', minor = "none") +  panel_border()+ theme(legend.position = "none", text = element_text(size = 20))
 
 evolBeta <- stats %>% dplyr::select(-X1) %>% ggplot(aes(evolBeta, group = .id, fill = .id)) + geom_density(alpha = 0.5) +  labs(x = "Scaled directional evolvability (Beta)", y = "Density") + scale_fill_manual(values = viridis(5), name = "Line", labels = c("Control t", "Upwards h'", "Upwards s'", "Downwards h", "Downwards s")) + background_grid(major = 'x', minor = "none") +  panel_border()+ theme(legend.position = "none", text = element_text(size = 20))
-BetaPC1 <- stats %>% dplyr::select(-X1) %>% ggplot(aes(BetaPC1, group = .id, fill = .id)) + geom_density(alpha = 0.5) +  labs(x = "Vector correlation of ", beta, " and the first eigenvector", y = "Density") + scale_fill_manual(values = viridis(5), name = "Line", labels = c("Control t", "Upwards h'", "Upwards s'", "Downwards h", "Downwards s")) + background_grid(major = 'x', minor = "none") +  panel_border()+ theme(legend.position = "none", text = element_text(size = 20))
 condevolBeta <- stats %>% dplyr::select(-X1) %>% ggplot(aes(condevolBeta, group = .id, fill = .id)) + geom_density(alpha = 0.5) +  labs(x = "Scaled directional condititonal evolvability (Beta)", y = "Density") + scale_fill_manual(values = viridis(5), name = "Line", labels = c("Control t", "Upwards h'", "Upwards s'", "Downwards h", "Downwards s")) + background_grid(major = 'x', minor = "none") +  panel_border()+ theme(legend.position = "none", text = element_text(size = 20))
+BetaPC1 <- stats %>% dplyr::select(-X1) %>% ggplot(aes(BetaPC1, group = .id, fill = .id)) + geom_density(alpha = 0.5) +  labs(x = expression(paste("Vector correlation of ", beta, " and the first eigenvector")), y = "Density") + scale_fill_manual(values = viridis(5), name = "Line", labels = c("Control t", "Upwards h'", "Upwards s'", "Downwards h", "Downwards s")) + background_grid(major = 'x', minor = "none") +  panel_border()+ theme(text = element_text(size = 20))
 
 figure_4 <- ggdraw() +
   draw_plot(evolDZ, 0, 0.5, 0.5, 0.5) +
@@ -59,8 +59,9 @@ figure_4 <- ggdraw() +
 figure_4_Beta <- ggdraw() +
   draw_plot(evolBeta, 0, 0.5, 0.5, 0.5) +
   draw_plot(condevolBeta, 0.5, 0.5, 0.5, 0.5) +
-  draw_plot(BetaPC1, 0, 0, 0.5, 0.5) +
+  draw_plot(BetaPC1, 0, 0, 0.7, 0.5) +
   draw_plot_label(c("A", "B", "C"), c(0, 0.5, 0), c(1, 1, 0.5), size = 20)
+save_plot("~/Dropbox/labbio/Shared Lab/Ratones_shared/figure4_beta.png", figure_4_Beta, ncol = 2, nrow = 2, base_aspect_ratio = 1.3, base_height = 4)
 
 betaDzComparison <- function(cov.matrix, line){
   beta_s <- solve(cov.matrix, delta_Z)
