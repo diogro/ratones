ec62805cc636899a991ceaa659a4d347eb39e486
Anna Penna <anna.penna@usp.com>
2017-04-06 17:37:08 -0400

    cvs files for all posterior matrices

---
+++ R/matrices_to_csv.R
@@ -0,0 +1,56 @@
+trait.names = colnames(raw.data)[10:44]
+g_matrices =llply(g_models, function(x) alply(x$Gs, 1) ) 
+p_matrices =llply(g_models, function(x) alply(x$Ps, 1) ) 
+
+matrices.to.csv = function(x, line.name, type)  
+{
+  y = x 
+  for (i in 1:length(x)) 
+  {
+    y[[i]] = as.data.frame(mutate(tbl_df(x[[i]]), matrix = paste(line.name, type, i, sep = "_"), trait = trait.names) )
+  } 
+  z = tbl_df(ldply(y))
+  z = z %>% select(matrix, trait, V1:V35)
+  colnames(z) = c("Matrix", "", trait.names)
+  return(z)
+}
+
+############################################################################################
+#                               G-Matrices                                                 #
+############################################################################################
+
+All.t.G = matrices.to.csv(p_matrices$control.t, line.name = "control_t", type = "G" )
+write.csv(All.t.G, file = "data/SI_Matrices/G/All_control_t_posterior_G.csv")
+
+All.s.G = matrices.to.csv(p_matrices$downwards.s, line.name = "downwards_s", type = "G" )
+write.csv(All.s.G, file = "data/SI_Matrices/G/All_downwards_s_posterior_G.csv")
+
+All.h.G = matrices.to.csv(p_matrices$downwards.h, line.name = "downwards_h", type = "G" )
+write.csv(All.h.G, file = "data/SI_Matrices/G/All_downwards_h_posterior_G.csv")
+
+All.sp.G = matrices.to.csv(p_matrices$`upwards.s'`, line.name = "upwards_s'", type = "G" )
+write.csv(All.sp.G, file = "data/SI_Matrices/G/All_upwards_s'_posterior_G.csv")
+
+All.hp.G = matrices.to.csv(p_matrices$`upwards.h'`, line.name = "upwards_h'", type = "G" )
+write.csv(All.hp.G, file = "data/SI_Matrices/G/All_upwards_h'_posterior_G.csv")
+
+############################################################################################
+#                               P-Matrices                                                 #
+############################################################################################
+
+All.t.P = matrices.to.csv(p_matrices$control.t, line.name = "control_t", type = "P" )
+write.csv(All.t.P, file = "data/SI_Matrices/P/All_control_t_posterior_P.csv")
+
+All.s.P = matrices.to.csv(p_matrices$downwards.s, line.name = "downwards_s", type = "P" )
+write.csv(All.s.P, file = "data/SI_Matrices/P/All_downwards_s_posterior_P.csv")
+
+All.h.P = matrices.to.csv(p_matrices$downwards.h, line.name = "downwards_h", type = "P" )
+write.csv(All.h.P, file = "data/SI_Matrices/P/All_downwards_h_posterior_P.csv")
+
+All.sp.P = matrices.to.csv(p_matrices$`upwards.s'`, line.name = "upwards_s'", type = "P" )
+write.csv(All.sp.P, file = "data/SI_Matrices/P/All_upwards_s'_posterior_P.csv")
+
+All.hp.P = matrices.to.csv(p_matrices$`upwards.h'`, line.name = "upwards_h'", type = "P" )
+write.csv(All.hp.P, file = "data/SI_Matrices/P/All_upwards_h'_posterior_P.csv")
+
+ic.mean.P.matrices %>% str
